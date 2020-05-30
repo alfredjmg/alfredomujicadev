c0aa0307a5ed9ff8036d94fa624a340c7e3ba58d
alfredjmg <alfredomujicagodoy@gmail.com>
2020-05-29 21:24:13 -0400

    udpate

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// document.onreadystatechange = () => {
+//     if (document.readyState === 'complete') {
+//         var header = document.getElementsByClassName("header")[0];
+//         console.log(header.style.height);
+//         console.log(height);
+//         document.getElementsByClassName("header")[0].style.height = height;
+//         header.style.height = window.innerHeight + 60;
+//         console.log(header.style.height);
+//     }
+//   };
